ba2adf0d118e0a5ad99edc42d52c189e1eb89972
Manuel Riezebosch <mriezebosch@gmail.com>
2014-10-27 10:28:04 +0100

    leeg unit test project

---
+++ ADCSB/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ADCSB
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
