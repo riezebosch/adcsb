394600fd8420fbe50d88fb2e7920a65c6f4b92a3
Manuel Riezebosch <mriezebosch@gmail.com>
2014-10-27 10:38:54 +0100

    linq als doel

--- ADCSB/UnitTest1.cs
+++ ADCSB/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 
 namespace ADCSB
 {
@@ -9,6 +10,29 @@ namespace ADCSB
         [TestMethod]
         public void TestMethod1()
         {
+            int[] items = { 1, 4, 3, 6, 34, 563, 234, 56, 234, 45, 234, 23, 234, 234, 234, 234 };
+            var query = from i in items
+                        where i > 17
+                        select i * 3;
+
+            foreach (var item in query)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int[] items = { 1, 4, 3, 6, 34, 563, 234, 56, 234, 45, 234, 23, 234, 234, 234, 234 };
+            var query = items
+                .Where(i => i < 17)
+                .Select(i => i * 3);
+
+            foreach (var item in query)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
