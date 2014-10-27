6253e32479df28bf0da7c61c21d3d3dc811c3509
Manuel Riezebosch <mriezebosch@gmail.com>
2014-10-27 14:03:35 +0100

    Auto Implemented Properties

---
+++ ADCSB/AutoImplementedProperties.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ADCSB
+{
+    [TestClass]
+    public class AutoImplementedProperties
+    {
+        // Auto-implemented property
+        public int MyProperty { get; private set; }
+
+
+        private int _myProperty2;
+        public int MyProperty2
+        {
+            get { return _myProperty2; }
+           // set { _myProperty2 = value; }
+        }
+
+        private int _myProperty3;
+        public void setProperty3(int value)
+        {
+            _myProperty3 = value;
+        }
+
+        public int getProperty3()
+        {
+            return _myProperty3;
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            foreach (var method in this.GetType().GetMethods(System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Public))
+            {
+                Console.WriteLine(method.Name);
+            }
+            
+        }
+
+        struct MyStruct
+        {
+            public int MyProperty { get; set; }
+
+            public MyStruct(int value) : this()
+            {
+                MyProperty = value;
+            }
+        }
+
+        [TestMethod]
+        public void AutoImplementedPropertiesMetStructs()
+        {
+            var s = new MyStruct
+            {
+                MyProperty = 23
+            };
+
+            Console.WriteLine(s.MyProperty);
+
+
+            MyStruct s2 = new MyStruct
+            {
+                MyProperty = 23
+            };
+
+            Assert.AreEqual(s, s2);
+        }
+    }
+}
--- ADCSB/InitializersDemo.cs
+++ ADCSB/InitializersDemo.cs
@@ -16,6 +16,23 @@ namespace ADCSB
             public int Leeftijd { get; set; }
 
             public IList<decimal> Declaraties { get; set; }
+
+            public override bool Equals(object obj)
+            {
+                if (!(obj is Persoon))
+                {
+                    return false;
+                }
+
+                var p2 = (Persoon)obj;
+
+                return this.Naam == p2.Naam && this.Leeftijd == p2.Leeftijd;
+            }
+
+            public override int GetHashCode()
+            {
+                return this.Naam.GetHashCode() ^ this.Leeftijd.GetHashCode();
+            }
         }
 
         [TestMethod]
@@ -46,6 +63,7 @@ namespace ADCSB
                         };
 
             var query2 = getallen.Select(getal => new { LottoNummer = getal });
+
             foreach (var item in query2)
             {
                 Console.WriteLine(item.LottoNummer);
@@ -80,6 +98,44 @@ namespace ADCSB
             // Dit mag dus niet, blijkbaar is de gegenereerde property
             // spontaan read-only geworden :S
             //p.Naam = "Puk";
+
+            var p2 = new { Naam = "Piet", Leeftijd = (short)25 };
+            Assert.AreEqual(p, p2);
+
+            // Door het omdraaien van de properties worden verschillende
+            // types gegenereeerd waardoor de equals nooit true kan opleveren.
+            var p3 = new { Leeftijd = (short)25, Naam = "Piet" };
+            Assert.AreNotEqual(p, p3);
+            
+
+            // Object is heel wat anders dan var
+            object p4 = new { Naam = "Piet", Leeftijd = (short)25 };
+
+            // Voorbeeldje van impliciete name
+            var p5 = new Persoon { Naam = "Manuel" };
+            var p6 = new { p5.Naam };
+            
+            // De naam van de property is impliciet overgenomen van het 
+            // Persoon object wat gebruikt is in de initializer.
+            Console.WriteLine(p6.Naam);
+
+            // Waarom ik fan ben van var
+            var items = new Dictionary<Tuple<string, int>, List<Persoon>>();
+            Dictionary<Tuple<string, int>, List<Persoon>> items2 = new Dictionary<Tuple<string, int>, List<Persoon>>();
+        }
+
+        [TestMethod]
+        public void VergelijkTweeObjecten()
+        {
+            var p3 = new Persoon { Naam = "Piet" };
+            var p4 = new Persoon { Naam = "Piet" };
+
+            Assert.AreEqual(p3, p4);
+        }
+
+        private static object Create()
+        {
+            return new { Aantal = 3};
         }
     }
 }
