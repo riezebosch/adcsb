b0ed88f7386530d458ddf2d356e151fd64be05ea
Manuel Riezebosch <mriezebosch@gmail.com>
2014-10-28 15:16:44 +0100

    async met progress en cancellation

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -23,21 +24,92 @@ namespace WindowsFormsApplication1
            
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private async void button1_Click(object sender, EventArgs e)
         {
             int n = int.Parse(textBox1.Text);
 
-            Task.Run(() => Fib(n)).ContinueWith(t => label1.Text = t.Result.ToString(), TaskScheduler.FromCurrentSynchronizationContext());
+            var source = new CancellationTokenSource();
+            var progress = new Progress<int>(i => label1.Text = "We zijn bij getal: " + i);
+            var fib = Task.Run(() => Fib(n, source.Token, progress));
+
+            await Task.WhenAny(fib, Task.Delay(5000));
+            if (fib.IsCompleted)
+            {
+                label1.Text = fib.Result.ToString();
+            }
+            else
+            {
+                source.Cancel();
+
+                MessageBox.Show("Duurt lang");
+            }
         }
 
-        private int Fib(int n)
+        private async Task<bool> IsHetAlVrijdag(IProgress<int> progress)
         {
+            //Thread.Sleep(20000);
+
+
+            for (int i = 0; i < 20; i++)
+            {
+                progress.Report(i);
+                await Task.Delay(1000); 
+            }
+
+            return DateTime.Today.DayOfWeek == DayOfWeek.Friday;
+        }
+
+        private int Fib(int n, CancellationToken token, IProgress<int> progress)
+        {
+            token.ThrowIfCancellationRequested();
+
+            // Slecht voorbeeld van Progress
+            // omdat dit direct weer de UI
+            // thread bezet houdt met een
+            // overvloed van berichtjes.
+            progress.Report(n);
+            
             if (n <= 1)
             {
                 return n;
             }
+            
+            return Fib(n - 1, token, progress) + 
+                Fib(n - 2, token, progress);
+        }
 
-            return Fib(n - 1) + Fib(n - 2);
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            var progress = new Progress<int>(i => label1.Text = i.ToString());
+            MessageBox.Show(string.Format("Vrijdag?: {0}", await IsHetAlVrijdag(progress)));
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                await ThrowException();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private async Task ThrowException()
+        {
+            await Task.Delay(5000);
+            throw new NotImplementedException();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var p = new Process();
+            p.StartInfo = new ProcessStartInfo("notepad");
+            p.Start();
+            
+            p.EnableRaisingEvents = true;
+            p.Exited += (s, a) => MessageBox.Show("Notepad is weer gesloten");
         }
     }
 }
