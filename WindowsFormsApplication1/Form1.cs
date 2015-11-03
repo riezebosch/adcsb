26537f39383d15fe7dc3ca17e4a36675640f4027
Manuel Riezebosch <mriezebosch@gmail.com>
2015-11-03 11:20:51 +0100

    demo van async await met fibonacci

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(textBox1.Text);
+            label1.Text = Fib(n).ToString();
+        }
+
+        private int Fib(int n)
+        {
+            if (n <= 1)
+                return n;
+
+            return Fib(n - 1) + Fib(n - 2);
+        }
+    }
+}
