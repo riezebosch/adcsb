6e6025a81dc3375e4c1e9695c04e24aabb5bde23
Manuel Riezebosch <mriezebosch@gmail.com>
2016-06-02 13:07:54 +0200

    Fibanacci, recursief met WPF

---
+++ ADCSB.Fib.Tests/FibHelpers.cs
@@ -0,0 +1,12 @@
+﻿namespace ADCSB.Fib.Tests
+{
+    class FibHelpers
+    {
+        public static int Fib(int n)
+        {
+            if (n <= 1)
+                return n;
+            return Fib(n - 1) + Fib(n - 2);
+        }
+    }
+}
---
+++ ADCSB.Fib.Tests/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace ADCSB.Fib.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Fib0()
+        {
+            int n = 0;
+            int fib = FibHelpers.Fib(n);
+            fib.ShouldBe(0);
+        }
+
+        [TestMethod]
+        public void Fib1()
+        {
+            int n = 1;
+            int fib = FibHelpers.Fib(n);
+            fib.ShouldBe(1);
+        }
+
+        [TestMethod]
+        public void Fib2()
+        {
+            int n = 2;
+            int fib = FibHelpers.Fib(2);
+            fib.ShouldBe(1);
+        }
+    }
+}
