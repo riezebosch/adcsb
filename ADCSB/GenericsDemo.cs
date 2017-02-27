1fc9a5f546239413ae222a55577e7c26ba7b4ac6
Manuel Riezebosch <mriezebosch@gmail.com>
2017-02-27 16:24:52 +0100

    co- en contra-variance

--- ADCSB/GenericsDemo.cs
+++ ADCSB/GenericsDemo.cs
@@ -140,6 +140,97 @@ namespace ADCSB
                 set { items[index] = value; }
             }
         }
+
+        [Fact]
+        public void InheritanceMetGenerics()
+        {
+            // Dit kan:
+            Person p = new Student();
+
+            List<Student> students = new List<Student>();
+            students.Add(new Student { Name = "Piet" });
+
+            List<Person> people = new List<Person>();
+            people.Add(new Student { Name = "Piet" });
+
+            // Dit mag natuurlijk niet:
+            //students.Add(new Person());
+
+            // Daarom kan dit ook niet:
+            //List<Person> people = students;
+            //people.Add(new Person());
+            //people.Add(new Student());
+
+            // Omgekeerd ook niet
+            //students = people;
+
+            // Omdat dit dan niet gegarandeerd is
+            //Student s = students[0];
+
+            // Dit kan allebei vanwege de co-variance van IEnumberale
+            PrintAllPeople(people);
+            PrintAllPeople(students);
+        }
+
+
+        private void PrintAllPeople(IEnumerable<Person> people)
+        {
+            foreach (var p in people)
+            {
+                output.WriteLine(p.Name);
+            }
+        }
+
+        interface IGenericCoVariance<out T>
+        {
+            T Method();
+
+            // Dit mag niet omdat "out T" alleen als return type gebruikt mag worden
+            //void Method(T input);
+        }
+
+        interface IGenericContraVariance<in T>
+        {
+            // Dit mag niet omdat "in T" alleen als argument gebruikt mag worden
+            //T Method();
+
+            void Method(T input);
+        }
+
+        class Person
+        {
+            public string Name { get; internal set; }
+        }
+
+        class Student : Person
+        {
+        }
+
+        [Fact]
+        public void GegenereerdeTypesMetGenerics()
+        {
+            var a = new Generic<int>();
+            var b = new Generic<int>();
+            var c = new Generic<char>();
+
+            Assert.Equal(2, a.Counter);
+            Assert.Equal(2, b.Counter);
+            Assert.Equal(1, c.Counter);
+        }
+
+        private class Generic<T>
+        {
+            // Demo mbv een static omdat die
+            // waarde over classes van hetzelfde type
+            // heen gedeeld wordt.
+            public static int counter;
+            public int Counter => counter;
+
+            public Generic()
+            {
+                counter++;
+            }
+        }
     }
 }
 
