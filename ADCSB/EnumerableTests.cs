c1137113e4e85e44b17abf6b66d371c0dee3ec75
Manuel Riezebosch <mriezebosch@gmail.com>
2017-02-27 14:13:00 +0100

    yield return

--- ADCSB/EnumerableTests.cs
+++ ADCSB/EnumerableTests.cs
@@ -1,14 +1,23 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 //using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace ADCSB
 {
     public class EnumerableTests
     {
+        private readonly ITestOutputHelper output;
+
+        public EnumerableTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
         [Fact]
         public void ItemsNotMatchingPredicateAreNotInResult()
         {
@@ -36,7 +45,7 @@ namespace ADCSB
 
         private class Enumerable
         {
-            public static IEnumerable<T> Where<T>(IEnumerable<T> items,  Where<T> where)
+            public static IEnumerable<T> Where<T>(IEnumerable<T> items, Where<T> where)
             {
                 // hint: yield return :)
                 foreach (var item in items)
@@ -55,6 +64,89 @@ namespace ADCSB
                     yield return select(item);
                 }
             }
+        }
+
+        [Fact]
+        public void YieldReturn()
+        {
+            int[] items = { 1, 2, 3, 4, 5, 6, 7, 9, 8 };
+
+            // Enumerable class zelf maken
+            // Where methode zelf make
+            // delegate om lambda mee te geven zelf maken
+            IEnumerable<int> result = EnumerableZonderYield.Where(items, i => i >= 2);
+
+            foreach (var item in result)
+            {
+                output.WriteLine(item.ToString());
+            }
+        }
+
+        private class EnumerableZonderYield
+        {
+            public static IEnumerable<T> Where<T>(IEnumerable<T> items, Where<T> where)
+            {
+                return new MyEnumerable<T>(items, where);
+            }
+
+            private class MyEnumerable<T> : IEnumerable<T>
+            {
+                private IEnumerator<T> items;
+                private Where<T> where;
+
+                public MyEnumerable(IEnumerable<T> items, Where<T> where)
+                {
+                    this.items = items.GetEnumerator();
+                    this.where = where;
+                }
+
+                public IEnumerator<T> GetEnumerator()
+                {
+                    return new MyEmerator<T>(items, where);
+                }
+
+                IEnumerator IEnumerable.GetEnumerator()
+                {
+                    return GetEnumerator();
+                }
+            }
+            private class MyEmerator<T> : IEnumerator<T>
+            {
+                private IEnumerator<T> items;
+                private Where<T> where;
+
+                public MyEmerator(IEnumerator<T> items, Where<T> where)
+                {
+                    this.items = items;
+                    this.where = where;
+                }
+
+                public T Current => items.Current;
+
+                object IEnumerator.Current => Current;
+
+                public void Dispose()
+                {
+                    items.Dispose();
+                }
+
+                public bool MoveNext()
+                {
+                    bool hasNext = items.MoveNext();
+                    while (hasNext && !where(items.Current))
+                    {
+                        hasNext = items.MoveNext();
+                    }
+
+                    return hasNext;
+                }
+
+                public void Reset()
+                {
+                    throw new NotImplementedException();
+                }
+            }
+
         }
     }
 }
